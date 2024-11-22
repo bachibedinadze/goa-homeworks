ac2a5084fb74493ab0f3066cd7b9b58e097622a2
bachibedinadze <ubralodninja@gmail.com>
2024-11-22 16:24:17 +0400

    commit

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,25 @@
+for i in range(20):
+    print("ბაჩი ბედინაძე")
+
+
+
+# მომხმარებლისგან რიცხვების მიღება
+first_number = int(input("შეიყვანეთ პირველი რიცხვი: "))
+second_number = int(input("შეიყვანეთ მეორე რიცხვი: "))
+
+# for ციკლი, რომელიც იტარებს პირველ და მეორე რიცხვებს შორის
+for i in range(first_number, second_number + 1):
+    print(i ** 2)  # ცვლადის კვადრატის დაბეჭდვა
+
+
+
+
+
+n = int(input("Enter a number: "))
+
+
+for i in range(1, n + 1):
+    print(f"The square of {i} is {i ** 2}")
+
+
+
---
+++ level 14/homework/homework.py
@@ -0,0 +1,3 @@
+coins = 36
+keys = 2
+print(coins > d keys > 0)
