012b1d98e9148df57067a516667a19e1c2a97453
bachibedinadze <ubralodninja@gmail.com>
2024-11-22 16:31:27 +0400

    commit

---
+++ level 02/homework/homework.py
@@ -0,0 +1,131 @@
+from turtle import *
+speed (30)
+width (7)
+color ("black")
+begin_fill()
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+end_fill()
+
+forward (70)
+left (90)
+color ("yellow")
+begin_fill()
+forward (120)
+right (90)
+forward (60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200,200)
+pendown()
+
+color("black")
+begin_fill()
+left (180)
+forward (50)
+left (90)
+forward(50)
+left (90)
+forward (50)
+end_fill()
+
+penup()
+goto(50,200)
+pendown()
+
+color=("black")
+begin_fill()
+left (180)
+forward (50)
+left (90)
+forward(50)
+left (90)
+forward (50)
+end_fill()
+
+penup()
+goto(200,250)
+pendown()
+begin_fill()
+
+right (150)
+forward (50)
+left (120)
+forward (50)
+
+end_fill()
+
+penup()
+goto(50,250)
+pendown()
+begin_fill()
+
+right(120)
+forward (50)
+left (120)
+forward (50)
+
+end_fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
---
+++ level 02/homework/homework1.py
@@ -0,0 +1,33 @@
+name = "bachi"
+surname = "bedinadze"
+age = 14
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+
+
+print(surname)
+print(surname)
+print(surname)
+print(surname)
+print(surname)
+print(surname)
+print(surname)
+print(surname)
+
+
+print(age)
+print(age)
+print(age)
+print(age)
+print(age)
+print(age)
+print(age)
+print(age)
+print(age)
+print(age)
