482a1b16c61cf0431f3051f56654c65d94cdc133
bachibedinadze <ubralodninja@gmail.com>
2024-12-03 15:10:23 +0400

    commit

---
+++ level 04/homeworks/classworks/classwork.py
@@ -0,0 +1,55 @@
+# String ცვლადები
+string1 = "Hello"
+string2 = "World"
+
+# Integer ცვლადები
+integer1 = 10
+integer2 = 5
+
+# Float ცვლადები
+float1 = 3.14
+float2 = 2.71
+
+# დაბეჭდვა ყველა ცვლადის ერთდროულად
+print(string1, string2, integer1, integer2, float1, float2)
+
+print(5 + 3)
+print(10 + 15)
+print(7 + 2.5)
+print(7 + 2.5)
+print(1, 2 +3, 4)
+
+
+print(10 - 5)
+print(20 - 7)
+print(15.5 - 5.5)
+print(22 - 7)
+print(20 - 10) 
+
+
+print(5 * 3)
+print(2.5 * 4)
+print(2.5 * 4)
+print([2 * 3)
+print(3 * 1.5)
+
+
+print(10 / 2)
+print(15 / 4)
+print(9 / 3)
+print(7 / 2)
+print(20 / 6)
+
+
+print(10 % 3)
+print(15 % 4)
+print(9 % 5)
+print(20 % 6)
+print(17 % 6)
+
+
+print(2 ** 3)
+print(5 ** 2)
+print(3 ** 4)
+print(2 ** 0)
+print(9 ** 0.5)
