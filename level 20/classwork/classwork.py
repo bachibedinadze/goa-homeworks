557a434615c16207f669bfc4407eb73c4911ee46
bachibedinadze <ubralodninja@gmail.com>
2024-12-14 19:12:32 +0400

    commit

---
+++ level 20/classwork/classwork.py
@@ -0,0 +1,17 @@
+# სიის შექმნა
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# ინდექსების გამოყენებით თითოეული ელემენტის ცვლადებში შენახვა
+element_1 = my_list[0]
+element_2 = my_list[1]
+element_3 = my_list[2]
+element_4 = my_list[3]
+element_5 = my_list[4]
+element_6 = my_list[5]
+element_7 = my_list[6]
+element_8 = my_list[7]
+element_9 = my_list[8]
+element_10 = my_list[9]
+
+# შედეგები
+print(element_1, element_2, element_3, element_4, element_5, element_6, element_7, element_8, element_9, element_10)
