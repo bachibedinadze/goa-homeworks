39b2d9d3cfe6a976483aa5d62a3e324315fd85ff
bachibedinadze <ubralodninja@gmail.com>
2025-07-12 10:06:20 +0300

    commit

---
+++ level 82/classwork/classwork1.js
@@ -0,0 +1,30 @@
+let dayNumber = parseInt(prompt("Enter a number (1–7):"));
+let dayName;
+
+switch (dayNumber) {
+  case 1:
+    dayName = "Monday";
+    break;
+  case 2:
+    dayName = "Tuesday";
+    break;
+  case 3:
+    dayName = "Wednesday";
+    break;
+  case 4:
+    dayName = "Thursday";
+    break;
+  case 5:
+    dayName = "Friday";
+    break;
+  case 6:
+    dayName = "Saturday";
+    break;
+  case 7:
+    dayName = "Sunday";
+    break;
+  default:
+    dayName = "Invalid number (must be 1–7)";
+}
+
+console.log(dayName);
