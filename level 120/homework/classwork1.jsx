900eac582a4fde26f5d4339ca1e672f44c773722
bachibedinadze <ubralodninja@gmail.com>
2025-11-22 19:10:12 +0400

    commit

---
+++ level 120/homework/classwork1.jsx
@@ -0,0 +1,10 @@
+function HeadingAndParagraph() {
+  return (
+    <div>
+      <h1>This is a heading</h1>
+      <p>This is a paragraph describing the heading.</p>
+    </div>
+  );
+}
+
+export default HeadingAndParagraph;
---
+++ level 120/homework/classwork3.jsx
@@ -0,0 +1,15 @@
+function TwoSectionLayout() {
+  return (
+    <div>
+      <div style={{ border: "1px solid #ccc", padding: "10px", marginBottom: "10px" }}>
+        <strong>Section 1:</strong> This is the first section.
+      </div>
+
+      <div style={{ border: "1px solid #ccc", padding: "10px" }}>
+        <strong>Section 2:</strong> This is the second section.
+      </div>
+    </div>
+  );
+}
+
+export default TwoSectionLayout;
