a3b854bcc68a2b5b671b458a22dae336dc735efa
bachibedinadze <ubralodninja@gmail.com>
2026-07-19 10:18:10 +0300

    commit

--- level 186/homework/phones/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.phone_list, name='phone_list'),
-    path('<int:id>/', views.phone_detail, name='phone_detail'),
-    path('delete/<int:id>/', views.phone_delete, name='phone_delete'),
-]
